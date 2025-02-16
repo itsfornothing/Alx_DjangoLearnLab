60f0e2e0ec7071b16441be778ac89ae48c491c21
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-16 10:45:50 +0300

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,5 +6,5 @@ admin.site.register(Book)
 
 
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('title', 'author', 'published_date')
+  list_display = ('title', 'author', 'publication_year')
   search_fields = ('title', 'author')
