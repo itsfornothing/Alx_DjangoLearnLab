31c810ae2795e6a398b8d4f6efffbcb979aec448
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-22 16:29:09 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,12 @@ from django.views.generic.detail import DetailView
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
 
 # Create your views here.
 def all_books(request):
@@ -17,6 +23,24 @@ class LibraryDetail(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User Not Found....")
+            return redirect("home")
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or Password does not match...")
+
+    return render(request, "login.html") 
    
 
 class SignUpView(CreateView):
