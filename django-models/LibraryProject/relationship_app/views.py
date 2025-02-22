4e8c17779ba71cb0f4e435edc29100cefa9d3716
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-22 16:30:13 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
 from django.contrib import messages
 
 
