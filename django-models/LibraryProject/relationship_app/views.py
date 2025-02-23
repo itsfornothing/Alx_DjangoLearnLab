0b743a28ecf08c11d259618718ca1f3ccb2f7610
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-23 15:11:26 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -57,9 +57,12 @@ class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logged_out.html"
 
 
-@user_passes_test(lambda user: user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin')
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, 'relationship_app/admin.html')
+    return render(request, 'admin.html', {'message': 'Welcome to Admin Dashb
 
 @user_passes_test(lambda user: user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian')
 def librarian_view(request):
