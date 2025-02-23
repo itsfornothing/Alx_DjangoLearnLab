835ad0e93945b7610bbd577bcb66124824ae5d86
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-23 16:45:41 +0300

    Update admin_view.py

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -7,9 +7,9 @@ def is_admin(user):
         return False
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-# Combine login_required and user_passes_test for better security
-@login_required
-@user_passes_test(is_admin, login_url='/login/')
+
+
+@user_passes_test(is_admin)
 def Admin(request):
     # Only Admin role users will reach this point
     context = {
