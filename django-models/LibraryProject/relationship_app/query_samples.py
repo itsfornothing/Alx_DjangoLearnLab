2b0e4c0c5072dc5af217d4115603f6bc3277db5d
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-23 13:15:44 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -9,7 +9,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # ✅ Query 1: Get all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
+    author = Author.objects.filter(author=author)
     return author.books.all()  # Uses the related_name 'books' from ForeignKey
 
 # ✅ Query 2: List all books in a library
