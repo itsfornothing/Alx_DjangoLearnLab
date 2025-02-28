3eb2f5a79471a41c3dc95e509e8b06f19d8bdc54
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 11:20:43 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import permission_required
+
 
 # Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
+def all_books(request):
+    books = Book.objects.all() 
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Assuming you have a book list view
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/edit_book.html', {'form': form, 'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/delete_book.html', {'book': book})
