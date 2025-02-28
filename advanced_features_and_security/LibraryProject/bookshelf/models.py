f315539f8f04798b9d734fddc79ff068b2a9414f
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 10:16:55 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.ImageField()
 
