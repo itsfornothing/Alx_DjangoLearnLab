d39e8cc181949708002be67f5a0b0e1b54d86a98
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 10:25:37 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"  
 LOGOUT_REDIRECT_URL = "/login/"  
 
-AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
