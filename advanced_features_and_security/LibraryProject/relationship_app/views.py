2d132b5c5a0d233635dcb92473b2bacfc1df804c
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 11:11:17 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -14,8 +14,8 @@ from .models import UserProfile
 from django.contrib.auth.decorators import permission_required
 
 
-
 # Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
 def all_books(request):
     books = Book.objects.all() 
     return render(request, "relationship_app/list_books.html", {"books": books})
@@ -84,7 +84,7 @@ def member_view(request):
 
 
 
-@permission_required('relationship_app.can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_create', raise_exception=True)
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
@@ -96,7 +96,7 @@ def add_book(request):
     return render(request, 'books/add_book.html', {'form': form})
 
 
-@permission_required('relationship_app.can_change_book', raise_exception=True)
+@permission_required('relationship_app.can_edit', raise_exception=True)
 def edit_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
@@ -108,7 +108,7 @@ def edit_book(request, pk):
         form = BookForm(instance=book)
     return render(request, 'books/edit_book.html', {'form': form, 'book': book})
 
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
+@permission_required('relationship_app.can_delete', raise_exception=True)
 def delete_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
