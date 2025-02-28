90a954f056a97c644f0c52fb1091a6452cb203db
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 11:12:39 +0300

    Update admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,11 +1,64 @@
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian, CustomUser
-
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'admin', 'staff', "date_of_birth", "profile_photo"]
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
 admin.site.register(CustomUser)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'admin', 'staff', "date_of_birth", "profile_photo"]
+
+
+Editors, created = Group.objects.get_or_create(name ='Editors')
+Viewers, created = Group.objects.get_or_create(name ='Viewers')
+Admins, created = Group.objects.get_or_create(name ='Admins')
+
+ct = ContentType.objects.get_for_model(Librarian)
+
+
+Admins_permissions = [
+    {'codename': 'can_view', 'name': 'Can View'},
+    {'codename': 'can_delete', 'name': 'Can Delete'},
+    {'codename': 'can_add', 'name': 'Can Add'},
+    {'codename': 'can_edit', 'name': 'Can Edit'}
+]
+
+for perm in Admins_permissions:
+    permission = Permission.objects.create(
+        codename=perm['codename'],
+        name=perm['name'],
+        content_type=ct
+    )
+    Admins.permissions.add(permission)
+
+
+
+Viewers_permissions = [
+    {'codename': 'can_view', 'name': 'Can View'},
+]
+
+for perm in Admins_permissions:
+    permission = Permission.objects.create(
+        codename=perm['codename'],
+        name=perm['name'],
+        content_type=ct
+    )
+    Viewers.permissions.add(permission)
+
+
+Editors_permissions = [
+    {'codename': 'can_view', 'name': 'Can View'},
+    {'codename': 'can_edit', 'name': 'Can Edit'}
+]
+
+for perm in Admins_permissions:
+    permission = Permission.objects.create(
+        codename=perm['codename'],
+        name=perm['name'],
+        content_type=ct
+    )
+    Editors.permissions.add(permission)
