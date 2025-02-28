64930298cf7796eb2ebc99e1a50b648d87fc601a
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-02-28 10:12:47 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
+
+    def create_user(self):
+        if not self.username:
+            raise ValueError("User must have username")
+        if not self.password:
+            raise ValueError("User must have a password")
+        
+        user = self.model(username=self.username, email=self.email)
+        user.set_password(self.password)
+        user.active = False
+        user.staff = False
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self):
+        if not self.username:
+            raise ValueError("User must have username")
+        if not self.password:
+            raise ValueError("User must have a password")
+        
+        user = self.model(username=self.username, email=self.email)
+        user.set_password(self.password)
+        user.active = True
+        user.staff = True
+        user.save(using=self._db)
+        return user
+    
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
