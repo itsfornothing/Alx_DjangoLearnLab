5a5c13eca1851f7427972d6e78ddbfadf0b015dd
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-15 16:33:53 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Create your views here.
+class BookList(generics.ListView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.DetailView):
+    queryset = Book.objects.get()
+    serializer_class = BookSerializer
+
+    def get(self, request, book_id):
+        try:
+            note = Book.objects.get(id=book_id)
+            serializer = self.serializer_class(note, context={'request': request})
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+
+
+class BookCreate(generics.CreateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdate(generics.UpdateView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def put(self, request, note_id):
+        try:
+            note = Book.objects.get(id=note_id, owner=request.user)
+            serializer = self.serializer_class(note, data=request.data, context={'request': request})
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'msg': 'Book updated successfully', 'data': serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+class BookDelete(generics.DeleteView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
