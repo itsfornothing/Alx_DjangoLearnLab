c3719aaa7fc39aa284cfcab5168ff68a100a0cfa
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-16 12:00:44 +0300

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'advanced_api_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',  # Use SQLite for testing
+        'NAME': ':memory:',  # In-memory test DB (faster)
     }
 }
 
