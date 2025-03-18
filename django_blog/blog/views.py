6b19d44e7ac38b571cc22cdae04d9a541b629e84
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-18 14:57:14 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from .form import UserForm, PostForm, CommentForm
+from .forms import UserForm, PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
