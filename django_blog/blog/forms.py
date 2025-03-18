f14e7c8bf08911022664b67547eb2a5b8133233d
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-18 16:01:11 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -58,7 +58,7 @@ class PostForm(ModelForm):
 
 
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["post", "author", "content", "created_at", "updated_at"]
