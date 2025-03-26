be176869bb989ba329d9eeb512c3c66876fabd92
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-26 18:20:16 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'rest_framework.authtoken',
+    'posts',
 ]
+
+REST_FRAMEWORK = { 
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination', 
+    'PAGE_SIZE': 2, 
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
