54e930dfd7fa26da5186e1fd90abbcec705e7c89
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-27 07:05:00 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,57 +1,84 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model, authenticate
-from rest_framework.authtoken.models import Token  # Import Token for authentication
+from .models import CustomUser
+from django.contrib.auth import authenticate
 
-User = get_user_model()  # Dynamically fetch the user model
-serializers.CharField()
-Token.objects.creat
-get_user_model().objects.create_user
-Token.objects.create
 
 
 class RegisterationSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
     username = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True,required=True)
+    bio = serializers.CharField()
 
     class Meta:
-        model = User
-        fields = ['email', 'username', 'password']
+        model = CustomUser
+        fields = ['email', 'username', 'password', 'bio', 'profile_picture']
+
 
     def validate(self, data):
-        if User.objects.filter(username=data['username']).exists():
+        if CustomUser.objects.filter(username=data['username']).exists():
             raise serializers.ValidationError({'username': 'Username already exists'})
         return data
-
+    
     def create(self, validated_data):
-        # Create user
-        user = User.objects.create_user(
+        user = CustomUser.objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        # Generate token
-        token, created = Token.objects.get_or_create(user=user)
-        return {'user': user, 'token': token.key}  # Return token along with user
+            email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
 
-
-class LoginSerializer(serializers.Serializer):  
+class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True,required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password']
+
 
     def validate(self, data):
         email = data.get('email')
         password = data.get('password')
-        user = User.objects.filter(email=email).first()
+        user = CustomUser.objects.filter(email=email).first()
 
         if user:
             user_authenticate = authenticate(username=user.username, password=password)
 
             if user_authenticate:
-                # Generate token on login
-                token, created = Token.objects.get_or_create(user=user_authenticate)
                 data['user'] = user_authenticate
-                data['token'] = token.key  # Return token in response
                 return data
-
+            
         raise serializers.ValidationError({'error': 'Invalid credentials'})
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['followers', 'following']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.filter(username=validated_data['username'])
+        you = self.context['request'].user
+
+        you.following.add(user)
+        you.save()
+
+        user.followers.add(you)
+        user.save()
+
+class UnfollowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['followers', 'following']
+
+    def create(self, validated_data):
+        user = CustomUser.objects.filter(username=validated_data['username'])
+        you = self.context['request'].user
+
+        you.following.remove(user)
+        you.save()
+
+        user.followers.remove(you)
+        user.save()
