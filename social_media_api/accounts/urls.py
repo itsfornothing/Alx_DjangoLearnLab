57c11ca0f3af3c487b7b57e721f6b979287d1f4f
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-27 07:05:47 +0300

    Update urls.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, FollowView, UnfollowView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name="register"),
     path('login/', LoginView.as_view(), name="login"),
-
+    path('login/', LoginView.as_view(), name="login"),
+    path('follow/<int:user_id>/', FollowView.as_view(), name="follow"),
+    path('unfollow/<int:user_id>/', UnfollowView.as_view(), name="unfollow"),
 ]
+
