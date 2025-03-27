0d262f0fe11732e4429e33e80bbb2f2f837d063d
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-27 07:02:55 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CreatePostView, CreateCommentView, PostDetailView, CommentDetailView, PostTitleSearchView
+from .views import CreatePostView, CreateCommentView, PostDetailView, CommentDetailView, PostTitleSearchView, FeedView
 
 urlpatterns = [
     path('posts/', CreatePostView.as_view(), name='create_post'),
     path('comments/', CreateCommentView.as_view(), name='create_comment'),
-    path('post/<int:post_id>', PostDetailView.as_view(), name='post_by_id'),
-    path('comment/<int:comment_id>', CommentDetailView.as_view(), name='post_by_id'),
+    path('post/<int:post_id>', PostDetailView.as_view(), name='post_detail'),
+    path('comment/<int:comment_id>', CommentDetailView.as_view(), name='comment_detail'),
     path('post/search/title/<str:title>', PostTitleSearchView.as_view(), name='note_by_title'),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
