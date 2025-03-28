61e670e55ea9e0eead3f2d10a1e07c6e4878d4ef
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-28 14:37:42 +0300

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -28,4 +28,49 @@ class CommentSerializer(serializers.ModelSerializer):
         
         return super().create(validated_data)
          
-            
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['post', 'liked_by']
+
+
+    def validate(self, data):
+        if Like.objects.filter(liked_by=self.context['request'].user).exists():
+            raise serializers.ValidationError({'username': 'You already liked the post.'})
+        return data
+    
+    def create(self, validated_data):
+        post = Post.objects.get(pk=self.context['post_id'])
+        like = Like(
+            post=post,
+            liked_by=self.context['request'].user)
+        like.save()
+
+        notification = Notification(
+                recipient=validated_data['post'].author,
+                actor=self.context['request'].user,
+                verb="liked your post.",
+                target_content_type=validated_data['post'],
+                target_object_id=validated_data['post'].id
+
+            )
+        notification.save()
+
+        return like
+    
+
+class UnlikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['post', 'liked_by']
+
+    def create(self, validated_data):
+        post = Post.objects.get(pk=self.context['post_id'])
+
+        if Like.objects.filter(liked_by=self.context['request'].user).exists():
+            unlike = Like.objects.filter(post=post)
+            unlike.liked_by.remove()
+            unlike.save()
+
+        return post
