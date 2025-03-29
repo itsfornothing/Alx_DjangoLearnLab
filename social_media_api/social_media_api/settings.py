36300de3841192fb5138654029979990c130e4a4
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-29 11:01:08 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g3)7ck@!h3slx-io&%mydwkpgb#e*_rcokwsoo@ki!5$uz&^wi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
