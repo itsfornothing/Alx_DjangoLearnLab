419842fa0251ba8e2b842887301fd42a26d7446c
Halid Mohamed <150917290+itsfornothing@users.noreply.github.com>
2025-03-29 11:07:32 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AlxDB',
+        'USER': 'root',
+        'PASSWORD': 'Haha123@&$',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
